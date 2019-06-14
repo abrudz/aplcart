2be94094c4809d01030b62b5b2062ee0add036a9
abrudz <a.brudz@gmail.com>
2019-06-14 13:32:55 +0100

    synchronise title and url with query

--- aplcart.js
+++ aplcart.js
@@ -1,20 +1,27 @@
 function esc(s){return s.replace(/[<>&'"]/g,x=>ESC[x])};
 var ESC={'<':'&lt;','>':'&gt;','&':'&amp;',"'":'&apos;','"':'&quot;'}
 var table;
+function setTit(t){document.title=t?"APLcart: "+t:"APLcart - Find your way in APL"};
+function setUrl(q){history.replaceState({},document.title,window.location.pathname+(q?"?q="+q:""))};
 $(document).ready(function(){
-  $.get("table.tsv",function(w){table=(esc(w)).split(/\r?\n/g).slice(1).map(w=>w.split('\t')).filter(w=>w!="");
-    s=(new URLSearchParams(window.location.search)).get("q")
+  $.get("table.tsv",function(w){
+    table=(esc(w)).split(/\r?\n/g).slice(1).map(w=>w.split('\t')).filter(w=>w!="");
+    s=new URLSearchParams(window.location.search)
+    q=s.get("q");
+    setTit(q);
     $('table').DataTable({
       data:table,
       paging:false,
       scrollY:"calc(100vh - 110px)",
       searchDelay:250,
-      search:{search:(s?s:"")},
-      "language":{"search": "query:"}
+      search:{search:(q?q:"")},
+      language:{search:"query:"}
     });
     $(".dataTables_scrollBody").attr("accesskey","z");
-    $("input").attr("accesskey","q");
-    $("input").focus();
+    $("input").focus().attr({
+      accesskey:"q",
+      onkeyup:'q=$("input").val();setTit(q);setUrl(q)'
+    });
+    $("body").addClass(null!=s.get("w")?"w":"")
   });
-  if(null!=(new URLSearchParams(window.location.search)).get("w")){$("body").addClass("w")};
 });
