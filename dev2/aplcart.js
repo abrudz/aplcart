f1cebf35a5509ab35cad15f31d3be73d424cdff6
abrudz <a.brudz@gmail.com>
2019-07-22 17:45:43 +0100

    optimise JS

--- dev2/aplcart.js
+++ dev2/aplcart.js
@@ -1,20 +1,17 @@
-function E(s){return s.replace(/[<>&'"]/g,function(x){return {'<':'&lt;','>':'&gt;','&':'&amp;',"'":'&apos;','"':'&quot;'}[x]})}
-function I(){
-  fetch("/table.tsv"
-  ).then(function(d){return d.text()}
-  ).then(function(d){t.innerHTML=E(d).replace(/.*/,'<tbody>').replace(/\r?\n/g,'<tr><td>').replace(/\t/g,'<td>')+'</tbody>'}
-  ).then(function(){
-      if("undefined"===typeof(URLSearchParams)){document.body.className='w';document.body.removeChild(w)}
-      else{var s=new URLSearchParams(window.location.search);document.body.className=null!=s.get("w")?"w":"";q.value=s.get("q")}
-      Q();Z();})}
-function Z(){z.textContent="Showing "+document.querySelectorAll("tr:not([hidden])").length+" of "+document.querySelectorAll("tr").length}
-function W(){document.body.classList.toggle('w');q.focus()}
-function X(){q.value="";Q();q.focus()}
-function C(){var qv=q.value;history.replaceState({},document.title,window.location.pathname+(qv?"?q="+encodeURIComponent(qv):""));q.focus()}
-function Q(){
-  var fw,ws=q.value.toLowerCase().split(' '),tr=t.getElementsByTagName("tr")
+E=s=>s.replace(/[<>&'"]/g,x=>({'<':'&lt;','>':'&gt;','&':'&amp;',"'":'&apos;','"':'&quot;'}[x]))
+F=_=>q.focus()
+I=_=>{
+  fetch("/table.tsv")
+  .then(d=>d.text())
+  .then(d=>{t.innerHTML=E(d).replace(/.*/,'<tbody>').replace(/\r?\n/g,'<tr><td>').replace(/\t/g,'<td>')+'</tbody>'
+  var s=new URLSearchParams(location.search);document.body.className=null!=s.get("w")?"w":"";q.value=s.get("q");Q();F()})}
+W=_=>{document.body.classList.toggle('w');F()}
+X=_=>{q.value="";Q();F()}
+C=_=>{var qv=q.value;history.replaceState({},document.title,location.pathname+(qv?"?q="+encodeURIComponent(qv):""));F()}
+Q=_=>{
+  var f,l=q.value.toLowerCase().split(' '),tr=t.rows,n=0
   for(var i=0;i<tr.length;i++){
     var s=tr[i].textContent.toLowerCase()
-    for(var j=0;j<ws.length;j++){fw=s.indexOf(ws[j])>-1;if(!fw)break}
-    tr[i].hidden=!fw}
-  Z()}
+    for(var j=0;j<l.length;j++){f=s.indexOf(l[j])>-1;if(!f)break}
+    n+=tr[i].hidden=!f}
+    z.textContent="Showing "+n+" of "+tr.length}
