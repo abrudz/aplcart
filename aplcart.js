961ece00086ea658825a81f61510d36a63ce5d0a
abrudz <a.brudz@gmail.com>
2020-01-06 12:13:43 +0000

    Avoid searching across cell boundaries

--- aplcart.js
+++ aplcart.js
@@ -4,11 +4,11 @@ I=_=>{
   var s=new URLSearchParams(location.search);b.className=0==s.get("w")?"w":0==s.get("b")?"b":""
   fetch("table.tsv").then(d=>d.text()).then(d=>{t.innerHTML=d.replace(/[<>&'"]/g,x=>({'<':'&lt;','>':'&gt;','&':'&amp;',"'":'&apos;','"':'&quot;'}[x])).replace(/.*/,'<tbody>').replace(/\r?\n/g,'<tr><td>').replace(/\t/g,'<td>')+'</tbody>';F(Q(q.value=s.get("q")))})}
 Q=_=>{
-  var f,l=q.value.toLowerCase().split(' '),tr=t.rows,n=0
-  for(var i=0;i<tr.length;i++){
-    var s=tr[i].textContent.toLowerCase()
+  var f,l=q.value.toLowerCase().split(' '),r=t.rows,n=0
+  for(var i=0;i<r.length;i++){
+    var s=[...r[i].cells].map(c=>c.textContent).join("\t").toLowerCase()
     for(var j=0;j<l.length;j++){f=s.indexOf(l[j])>-1;if(!f)break}
-    n+=!(tr[i].hidden=!f)}
-  z.textContent="Showing "+n+" of "+tr.length}
+    n+=!(r[i].hidden=!f)}
+  z.textContent="Showing "+n+" of "+r.length}
 W=x=>F(b.className=x?x:"wb"[1+"wb".indexOf(b.className[0])])
 X=_=>F(Q(q.value=""))
