7eb8ef3c9af882f9b5ebb58fee579a1573495f8a
Adám Brudzewsky <a.brudz@gmail.com>
2020-06-28 18:34:35 +0100

    modern extensions, fix bleed between parts, fix shadown elements

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -8,11 +8,11 @@ I=_=>{
     Q1();Q2()})}
 E=x=>(["any","a numeric","an integer","a Boolean","a character"]["XMIACYNJBD".indexOf(x[0])%5]+(x[1]?[" scalar"," vector"," matrix"]["svm".indexOf(x[1])]:" array")+(x?" <code>"+x+"</code>":""))
 Q1=_=>{
-  c=(x=>x[Math.floor(Math.random()*x.length)])(ps);a=c[0].match(/^[a-z]*/i)[0];w=c[0].match(/[a-z]*$/i)[0]
-  q1.innerHTML=`Write a ${c[3].match(/\w+/)[0].toLowerCase()} ${c[5].toLowerCase()} function, <code>${a?a+" ":""}YourFunction ${w}</code>, that for ${a?E(a)+" and ":""}${E(w)}, solves the following ${["easy","medium-difficulty","hard"][(x=>Math.floor(3*x.indexOf(c)/x.length))(ps.filter(x=>x[2]===c[2]))]} task:<p>${c[1]}</p>`
+  c1=(x=>x[Math.floor(Math.random()*x.length)])(ps);a=c1[0].match(/^[a-z]*/i)[0];w=c1[0].match(/[a-z]*$/i)[0]
+  q1.innerHTML=`Write a ${c1[3].match(/\w+/)[0].toLowerCase()} ${c1[5].toLowerCase()} function, <code>${a?a+" ":""}YourFunction ${w}</code>, that for ${a?E(a)+" and ":""}${E(w)}, solves the following ${["easy","medium-difficulty","hard"][(x=>Math.floor(3*x.indexOf(c1)/x.length))(ps.filter(x=>x[2]===c1[2]))]} task:<p>${c1[1]}</p>`
 }
 Q2=_=>{
-  c=(x=>x[Math.floor(Math.random()*x.length)])(ps);a=c[0].match(/^[a-z]*/i)[0];w=c[0].match(/[a-z]*$/i)[0]
-  q2.innerHTML=`What does the following ${c[3].match(/\w+/)[0].toLowerCase()} ${c[5].toLowerCase()} function compute when given ${a?E(a)+" and ":""}${E(w)}?<p><code>${c[0]}</code></p>`
+  c2=(x=>x[Math.floor(Math.random()*x.length)])(ps);a=c2[0].match(/^[a-z]*/i)[0];w=c2[0].match(/[a-z]*$/i)[0]
+  q2.innerHTML=`What does the following ${c2[3].match(/\w+/)[0].toLowerCase()} ${c2[5].toLowerCase()} function compute when given ${a?E(a)+" and ":""}${E(w)}?<p><code>${c2[0]}</code></p>`
 }
 W=x=>b.className=x?x:"wb"[1+"wb".indexOf(b.className[0])]
