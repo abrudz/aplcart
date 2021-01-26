660ac54de384ee62ff6ca52612638d993830f6f5
Adám Brudzewsky <a.brudz@gmail.com>
2021-01-26 18:29:44 +0000

    enable single-tab mode

--- aplcart.js
+++ aplcart.js
@@ -4,6 +4,7 @@ I=_=>{
   var s=new URLSearchParams(location.search)
   b.className=0==s.get("w")?"w":0==s.get("b")?"b":""
   um.href="/quiz?"+b.className
+  if(s.get("self")!=null){document.querySelectorAll("[target]").forEach(e=>e.target="_self");um.href+="&self"}
   fetch("table.tsv").then(d=>d.text()).then(d=>{
     ps=d.replace(/[<>&'"]/g,x=>({'<':'&lt;','>':'&gt;','&':'&amp;',"'":'&apos;','"':'&quot;'}[x])).split(/\r?\n/g).splice(1).map(r=>r.split("\t"))
     c=ps.map(r=>r[0])
