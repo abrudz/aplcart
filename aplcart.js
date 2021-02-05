5a6d00a209ac2c1bdacdddde879730176d34725e
Adám Brudzewsky <a.brudz@gmail.com>
2021-02-05 08:34:12 +0000

    fall back to popup if clipboard unavailable

--- aplcart.js
+++ aplcart.js
@@ -1,6 +1,6 @@
 C=_=>{
   history.replaceState({},document.title,location.pathname+(q.value?"?q="+encodeURIComponent(q.value):""))
-  F(navigator.clipboard.writeText(location))
+  F(navigator.clipboard.writeText(location).catch(_=>prompt("Query URL:",location)))
 }
 F=_=>q.focus()
 I=_=>{
